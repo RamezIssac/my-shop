fca58e0c51f3bbfdbdd302ab2731a88d5be96018
Ramez Ashraf <ramez@kuwaitnet.com>
2023-05-11 21:36:31 +0300

    Percentage to total report

--- sales/reports.py
+++ sales/reports.py
@@ -13,8 +13,8 @@ from .models import Sale, Product
 class SaleListReportView(ReportView):
     report_model = Sale
     base_model = Sale
-    report_title = 'Sales List'
-    columns = ['number', 'date', 'client', 'product', 'quantity', 'price', 'value']
+    report_title = "Sales List"
+    columns = ["number", "date", "client", "product", "quantity", "price", "value"]
 
 
 @register_report_view
@@ -24,15 +24,18 @@ class TotalProductSales(ReportView):
     base_model = Product
 
     # the field to use for the date
-    date_field = 'date'
+    date_field = "date"
 
-    report_title = 'Total Product Sales'
+    report_title = "Total Product Sales"
 
     # the field to use for the group by
-    group_by = 'product'
+    group_by = "product"
 
     # Columns to display ,
-    columns = ['name', SlickReportField.create(Sum, 'value', verbose_name='Total Value')]
+    columns = [
+        "name",
+        SlickReportField.create(Sum, "value", verbose_name="Total Value"),
+    ]
 
 
 @register_report_view
@@ -42,17 +45,19 @@ class TotalProductSalesTimeSeries(ReportView):
     base_model = Product
 
     # the field to use for the date
-    date_field = 'date'
+    date_field = "date"
 
-    report_title = 'Product SalesTime Series'
+    report_title = "Product SalesTime Series"
 
     # the field to use for the group by
-    group_by = 'product'
+    group_by = "product"
 
     # Columns to display ,
-    columns = ['name',
-               '__time_series__',
-               SlickReportField.create(Sum, 'value', verbose_name='Total Value')]
+    columns = [
+        "name",
+        "__time_series__",
+        SlickReportField.create(Sum, "value", verbose_name="Total Value"),
+    ]
 
     time_series_selector = True
     time_series_selector_default = "monthly"
@@ -69,7 +74,7 @@ class TotalProductSalesTimeSeries(ReportView):
         },
         {
             "title": "Total",
-            'engine_name': 'chartsjs',
+            "engine_name": "chartsjs",
             "type": "column",
             "data_source": ["value"],
             "title_source": "type",
@@ -77,11 +82,98 @@ class TotalProductSalesTimeSeries(ReportView):
         },
         {
             "title": "Total",
-            'engine_name': 'chartsjs',
+            "engine_name": "chartsjs",
             "type": "pie",
             "data_source": ["value"],
             "title_source": "type",
             "plot_total": True,
         },
+    ]
 
+
+class TotalValue(SlickReportField):
+    calculation_method = Sum
+    calculation_field = "value"
+    is_summable = True
+    verbose_name = _("Total Value")
+    name = "total_value"
+
+    # def final_calculation(self, debit, credit, dep_dict):
+    #     result = super().final_calculation(debit, credit, dep_dict)
+    #     print(result)
+    #     return round(result, 2)
+
+
+class PercentageToTotal(SlickReportField):
+    requires = (TotalValue,)
+    prevent_group_by = True
+    is_summable = False
+    name = "percentage_to_total"
+    verbose_name = _("% Total")
+
+    @classmethod
+    def get_time_series_field_verbose_name(cls, date_period, index, dates, pattern):
+        return "%"
+        # return super().get_time_series_field_verbose_name(
+        #     date_period, index, dates, pattern
+        # )
+
+    def final_calculation(self, debit, credit, dep_dict):
+        try:
+            return round((dep_dict.get("total_value", 0) / (debit - credit)) * 100, 2)
+        except ZeroDivisionError:
+            return 0
+
+
+@register_report_view
+class TotalProductSalesTimeSeriesWithPercentage(ReportView):
+    report_model = Sale
+    base_model = Product
+
+    # the field to use for the date
+    date_field = "date"
+
+    report_title = "Product Sales Time Series (with %)"
+
+    # the field to use for the group by
+    group_by = "product"
+
+    # Columns to display ,
+    columns = [
+        "name",
+        "__time_series__",
+        TotalValue,
+        PercentageToTotal,
+    ]
+
+    time_series_selector = True
+    time_series_selector_default = "monthly"
+    #
+    time_series_columns = [
+        TotalValue,
+        PercentageToTotal,
+    ]
+
+    chart_settings = [
+        {
+            "type": "column",
+            "data_source": ["value"],
+            "title_source": "type",
+        },
+        {
+            "title": "Total",
+            "engine_name": "chartsjs",
+            "type": "column",
+            "data_source": ["value"],
+            "title_source": "type",
+            "plot_total": True,
+        },
+        {
+            "title": "Total",
+            "engine_name": "chartsjs",
+            "type": "pie",
+            "data_source": ["value"],
+            "title_source": "type",
+            "plot_total": True,
+        },
     ]
