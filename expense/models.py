e9bf36afe3a10b59686ffed619674bfa7ff8607e
Ramez Ashraf <ramez@kuwaitnet.com>
2023-05-13 16:10:23 +0300

    Change Credits on highcharts

--- expense/models.py
+++ expense/models.py
@@ -6,23 +6,25 @@ class Expense(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-        verbose_name = _('Expense')
-        verbose_name_plural = _('Expenses')
+        verbose_name = _("Expense")
+        verbose_name_plural = _("Expenses")
 
     def __str__(self):
         return self.name
 
 
 class ExpenseTransaction(models.Model):
-    number = models.CharField(max_length=100, verbose_name='Expense Transaction #')
+    number = models.CharField(max_length=100, verbose_name="Expense Transaction #")
     date = models.DateTimeField()
     notes = models.TextField(blank=True, null=True)
-    expense = models.ForeignKey(Expense, on_delete=models.PROTECT, verbose_name=_('Expense'))
-    value = models.DecimalField(max_digits=9, decimal_places=2, editable=False)
+    expense = models.ForeignKey(
+        Expense, on_delete=models.PROTECT, verbose_name=_("Expense")
+    )
+    value = models.DecimalField(max_digits=9, decimal_places=2)
 
     class Meta:
-        verbose_name = _('Expense Transaction')
-        verbose_name_plural = _('Expense Transactions')
+        verbose_name = _("Expense Transaction")
+        verbose_name_plural = _("Expense Transactions")
 
     def __str__(self):
         return f"{self.number} - {self.date}"
